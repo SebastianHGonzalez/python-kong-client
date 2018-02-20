607af0f702d1480d85d0b4f7eaa5b5db6d8d416b
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-20 15:29:28 -0300

    Added tests

--- tests/plugin_admin_tests.py
+++ tests/plugin_admin_tests.py
@@ -11,9 +11,17 @@ class PluginAdminTest(unittest.TestCase):
     def setUp(self):
         self.faker = faker.Faker()
 
+        self.plugin_created_at = self.faker.random_int()
+        self.plugin_id = self.faker.uuid4()
         self.plugin_name = self.faker.word()
         self.consumer_id = self.faker.uuid4()
         self.api_name_or_id = self.faker.uuid4()
+
+        self.plugin_json = {"created_at": self.plugin_created_at,
+                            "config": {"setting": "value"},
+                            "id": self.plugin_id,
+                            "enabled": True,
+                            "name": self.plugin_name}
 
         self.kong_url = self.faker.url()
         self.plugins_endpoint = self.kong_url + 'plugins/'
@@ -21,6 +29,7 @@ class PluginAdminTest(unittest.TestCase):
         self.plugin_admin_client = PluginAdminClient(self.kong_url, session=self.session_mock)
 
         self.session_mock.post.return_value.status_code = 201
+        self.session_mock.get.return_value.status_code = 200
 
     def test_create_plugin_for_all_apis_and_consumers(self):
 
@@ -62,3 +71,19 @@ class PluginAdminTest(unittest.TestCase):
         expected_data = {'name': self.plugin_name,
                          'config.setting': 'value'}
         self.session_mock.post.assert_called_once_with(self.plugins_endpoint, data=expected_data)
+
+    def test_retrieve_existing_plugin(self):
+        # Exercise
+        self.plugin_admin_client.retrieve(self.plugin_id)
+
+        # Verify
+        self.session_mock.get.assert_called_once_with(self.plugins_endpoint + self.plugin_id)
+
+    def test_retrieve_non_existing_plugin(self):
+        # Setup
+        self.session_mock.get.return_value.status_code = 404
+        self.session_mock.get.return_value.content = {"message": "Not found"}
+
+        # Verify
+        self.assertRaisesRegex(NameError, 'Not found',
+                               lambda: self.plugin_admin_client.retrieve(self.plugin_id))
