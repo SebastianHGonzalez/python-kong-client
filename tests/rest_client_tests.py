cc6cda9e4fbb545202d5eccef3145e7dcbc22081
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-03-13 13:29:28 -0300

    BugFix: now urls are validated before use to avoid bad requests
        refs:#9
    
        refs:#9

---
+++ tests/rest_client_tests.py
@@ -0,0 +1,36 @@
+import unittest
+
+from kong.kong_clients import RestClient
+
+
+class RestClientTest(unittest.TestCase):
+
+    def test_normalize_url_wo_trailing_slash(self):
+        # Setup
+        url = 'http://foo.bar'
+
+        # Exercise
+        normalized_url = RestClient.normalize_url(url)
+
+        # Validate
+        self.assertEqual('http://foo.bar/', normalized_url)
+
+    def test_normalize_url_wo_scheme(self):
+        # Setup
+        url = 'foo.bar/'
+
+        # Exercise
+        normalized_url = RestClient.normalize_url(url)
+
+        # Validate
+        self.assertEqual('http://foo.bar/', normalized_url)
+
+    def test_normalize_url_w_path_wo_trailing_slash(self):
+        # Setup
+        url = 'http://foo.bar/endpoint'
+
+        # Exercise
+        normalized_url = RestClient.normalize_url(url)
+
+        # Validate
+        self.assertEqual('http://foo.bar/endpoint/', normalized_url)
