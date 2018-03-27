3e9201cfd5557aa05e577813a1f63739a7554af7
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-03-27 15:35:17 -0300

    Refactor: Restructured test classes, added create w invalid params
        refs:#25

--- tests/service_admin_tests.py
+++ tests/service_admin_tests.py
@@ -6,6 +6,7 @@ from unittest.mock import MagicMock
 
 
 from kong.kong_clients import ServiceAdminClient
+from kong.exceptions import SchemaViolation
 
 
 class ServiceAdminClientAbstractTest:
@@ -18,7 +19,10 @@ class ServiceAdminClientAbstractTest:
     def new_service_admin_client(self):
         pass
 
-    def setUp(self):
+    def setup_client_var(self):
+        self.service_admin_client = self.new_service_admin_client()
+
+    def setup_service_vars(self):
         self.service_protocol = 'http'
         self.service_host = 'example.org'
         self.service_port = 8080
@@ -30,6 +34,50 @@ class ServiceAdminClientAbstractTest:
                                              self.service_path)
         self.service_retries = '5'
 
+    def test_create_a_service_w_url(self):
+        created = self.service_admin_client.create(name=self.service_name,
+                                                   url=self.service_url)
+
+        self.assert_correctly_created(created)
+
+    def assert_correctly_created(self, created):
+        self.assertEquals(12, len(created))
+        self.assertRegex(created['id'], '^[\w\d]{8}-([\w\d]{4}-){3}[\w\d]{12}$')
+        self.assertTrue(isinstance(created['created_at'], int))
+        self.assertTrue(isinstance(created['updated_at'], int))
+        self.assertTrue(isinstance(created['connect_timeout'], int))
+        self.assertRegex(created['protocol'], self.service_protocol)
+        self.assertRegex(created['host'], self.service_host)
+        self.assertTrue(isinstance(created['port'], int))
+        self.assertRegex(created['path'], self.service_path)
+        self.assertRegex(created['name'], self.service_name)
+        self.assertTrue(isinstance(created['retries'], int))
+        self.assertTrue(isinstance(created['read_timeout'], int))
+        self.assertTrue(isinstance(created['write_timeout'], int))
+
+    def test_create_service(self):
+        created = self.service_admin_client.create(name=self.service_name,
+                                                   protocol=self.service_protocol,
+                                                   host=self.service_host,
+                                                   port=self.service_port,
+                                                   path=self.service_path)
+        self.assert_correctly_created(created)
+
+    def test_create_w_invalid_params(self):
+        self.assertRaises(SchemaViolation, lambda:
+                          self.service_admin_client.create(name=self.service_name,
+                                                           url=self.service_url,
+                                                           invalid='invalid')
+                          )
+
+
+class ServiceAdminClientMockedTest(ServiceAdminClientAbstractTest, unittest.TestCase):
+
+    @property
+    def kong_url(self):
+        return 'http://kong.url/'
+
+    def setup_mock_vars(self):
         self.service_dict = {
             "id": "4e13f54a-bbf1-47a8-8777-255fed7116f2",
             "created_at": 1488869076800,
@@ -43,46 +91,35 @@ class ServiceAdminClientAbstractTest:
             "retries": 5,
             "read_timeout": 60000,
             "write_timeout": 60000
-            }
+        }
 
         self.session = MagicMock()
         self.session.post.return_value.status_code = 201
         self.session.post.return_value.json.return_value = self.service_dict
 
-        self.service_admin_client = self.new_service_admin_client()
-
-    def create_a_service_w_url(self):
-        return self.service_admin_client.create(name=self.service_name,
-                                                url=self.service_url)
-
-    def test_create_a_service_w_url(self):
-        created = self.create_a_service_w_url()
-
-        self.assertRegex(created['id'], '^[\w\d]{8}-([\w\d]{4}-){3}[\w\d]{12}$')
-        self.assertTrue(isinstance(created['created_at'], int))
-        self.assertTrue(isinstance(created['updated_at'], int))
-        self.assertTrue(isinstance(created['connect_timeout'], int))
-        self.assertRegex(created['protocol'], self.service_protocol)
-        self.assertRegex(created['host'], self.service_host)
-        self.assertTrue(isinstance(created['port'], int))
-        self.assertRegex(created['path'], self.service_path)
-        self.assertRegex(created['name'], self.service_name)
-        self.assertTrue(isinstance(created['retries'], int))
-
-
-class ServiceAdminClientMockedTest(ServiceAdminClientAbstractTest, unittest.TestCase):
-
-    @property
-    def kong_url(self):
-        return 'http://kong.url/'
-
     def new_service_admin_client(self):
         return ServiceAdminClient(self.kong_url, _session=self.session)
+
+    def setUp(self):
+        self.setup_service_vars()
+        self.setup_mock_vars()
+        self.setup_client_var()
 
     def test_create_a_service_w_url(self):
         super(ServiceAdminClientMockedTest, self).test_create_a_service_w_url()
 
-        expected_data = {'url': self.service_url, 'name': self.service_name}
+        expected_data = dict(name=self.service_name, url=self.service_url)
+        endpoint = self.kong_url + 'services/'
+        self.session.post.assert_called_once_with(endpoint, data=expected_data)
+
+    def test_create_service(self):
+        super(ServiceAdminClientMockedTest, self).test_create_service()
+
+        expected_data = dict(name=self.service_name,
+                             protocol=self.service_protocol,
+                             host=self.service_host,
+                             port=self.service_port,
+                             path=self.service_path)
         endpoint = self.kong_url + 'services/'
         self.session.post.assert_called_once_with(endpoint, data=expected_data)
 
@@ -92,6 +129,10 @@ class ServiceAdminClientServerTest(ServiceAdminClientAbstractTest, unittest.Test
     @property
     def kong_url(self):
         return 'http://localhost:8001/'
+
+    def setUp(self):
+        self.setup_service_vars()
+        self.setup_client_var()
 
     def new_service_admin_client(self):
         return ServiceAdminClient(self.kong_url, _session=requests.session())
